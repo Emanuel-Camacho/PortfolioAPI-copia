e2515ea1a452874edf3c9381430d01cf3d0683c5
pablopaezsheridan <pablopaezps97@gmail.com>
2025-12-06 16:49:11 -0300

    Role in user and comments in pipeline

--- PortfolioAPI/Data/Entities/User.cs
+++ PortfolioAPI/Data/Entities/User.cs
@@ -10,5 +10,6 @@ namespace PortfolioAPI.Data.Entities
         public int Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
+        public string Role { get; set; } = "User";
     }
 }
